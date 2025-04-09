2f29586be0bdebf6f9bdcc7e7fecec2183f02e0d
Haydn Trigg <me@haydntrigg.com>
2025-04-09 22:19:15 +0930

    Fix SoundStreamer Again

--- Code/SoundStreamer/Subroutine0.cpp
+++ Code/SoundStreamer/Subroutine0.cpp
@@ -1,1 +1,11 @@
 #include "Export/Subroutine/code_0.inl" 
+
+extern "C" _naked _weak void sub_6EF5310() // ___memcpy_0
+{
+    asm("jmp _memcpy;");
+}
+
+extern "C" _naked _weak void sub_6EF46E0() // ___memcpy
+{
+    asm("jmp _memcpy;");
+}
