cda55ca6201b229a0c2a8913c2533f79ee61e792
Haydn Trigg <me@haydntrigg.com>
2025-04-09 16:26:48 +0930

    Excalibur v2: SoundManager Dump

--- Code/SoundManager/Export/Exports/entry_point.h
+++
@@ -1,1 +0,0 @@
-#define SoundManager_entry_point _RelativeAddress(public_6ee8ed1, 0x06ee8ed1)
---
+++ Code/SoundManager/Export/Metadata.cpp
@@ -0,0 +1,7 @@
+#ifndef __INTELLISENSE__
+
+extern "C" size_t __base_address = 0x6EE0000;
+extern "C" void _sub_6EE8ED1();
+extern "C" void* __entry_ptr = reinterpret_cast<void*>(&_sub_6EE8ED1);
+
+#endif // __INTELLISENSE__
